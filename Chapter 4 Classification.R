2096f694b185573bea027f5a766f4a95a5d20092
sagar <akc14@hotmail.com>
2016-02-08 00:05:45 +0530

    Solution

---
+++ Chapter 4 Classification.R
@@ -0,0 +1,9 @@
+#Problem 1: Using a little bit of algebra, prove that (4.2) is equivalent to (4.3). In other words, the logistic function representation and logit representation for the logistic regression model are equivalent.
+
+
+# Problem 2: It was stated in the text that classifying an observation to the class for which (4.12) is largest is equivalent to classifying an observation to the class for which (4.13) is largest. Prove that this is the case. In other words, under the assumption that the observations in the kth class are drawn from a N (μ k , σ 2 ) distribution, the Bayes’ classifier assigns an observation to the class for which the discriminant function is maximized.
+
+
+
+
+
